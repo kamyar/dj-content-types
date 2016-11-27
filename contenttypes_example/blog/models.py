20f0809a6eedb6b5d00641a21ef25b5f11434ca6
Kamyar Ghasemlou <k.ghasemlou@gmail.com>
2016-11-27 15:59:12 +0300

    add blog artcile and photo models

--- contenttypes_example/blog/models.py
+++ contenttypes_example/blog/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
-# Create your models here.
+# Assign the User model in case it has been "swapped"
+User = settings.AUTH_USER_MODEL
+
+
+class Tag(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    term = models.CharField(max_length=30)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    tags = GenericRelation('Tag')
+
+    title = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+
+
+    class Meta:
+        ordering = ('created',)
+
+
+
+class Photo(models.Model):
+    author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    tags = GenericRelation('Tag')
+
+    image = models.ImageField()
+    title = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ('created',)
